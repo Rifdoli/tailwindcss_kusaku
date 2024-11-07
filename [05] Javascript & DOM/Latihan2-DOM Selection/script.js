b58acc7c0d743c4b1fc92adbb852eb08c81a2e2f
Rifdoli <rifqi.fadhlillah93@gmail.com>
2024-11-07 14:04:24 +0800

    Menambahkan script js baru manipulation

--- [05] Javascript & DOM/Latihan2-DOM Selection/script.js
+++ [05] Javascript & DOM/Latihan2-DOM Selection/script.js
@@ -42,31 +42,6 @@ p1[0].innerHTML = 'Ini diubah dari js'; */
 
 // p4.style.backgroundColor = 'orange';
 
-// element.innerHTML
-// const judul = document.getElementById('judul');
-// judul.innerHTML = '<em>Fadhlillah</em>';
-
-// const sectionA =document.querySelector('section#a');
-// sectionA.innerHTML = '<div><p>paragraf1</p></div>';
-
-// element.style.<properties>
-// const judul = document.querySelector('#judul');
-
-// judul.style.color = 'lightblue';
-// judul.style.backgroundColor = 'salmon';
-
-// element.setAttribute() atau element.getAttribute() atau element.removeAttribute()
-// const judul = document.getElementsByTagName('h1')[0];
-// judul.setAttribute ('name', 'Fadhlillah');
-
-// const a = document.querySelector('section#a a');
-
-// element.classlist()
-// const p2 = document.querySelector('.p2');
-// p2.classList.add('label');
-
-
-
 
 
 
