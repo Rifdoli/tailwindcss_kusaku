08e8c0b8cbd03e2a2615d7e06a4e2f8b4bd163d9
Rifdoli <rifqi.fadhlillah93@gmail.com>
2024-11-08 01:02:05 +0800

    menambahkan dokumen script event

---
+++ [05] Javascript & DOM/Latihan2-DOM Selection/script3_event.js
@@ -0,0 +1,8 @@
+const p3 = document.querySelector ('.p3');
+
+function ubahWarna(){
+    p3.style.backgorundcolor = 'lightblue';
+}
+
+const p2 = document.querySelector ('.p2');
+p2.onclick = ubahWarna;
