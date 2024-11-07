3855919a165eabedb980ed7de75a1c433e35d721
Rifdoli <rifqi.fadhlillah93@gmail.com>
2024-11-07 11:13:23 +0800

    Memperbarui script pada DOM Selection

--- [05] Javascript & DOM/Latihan2-DOM Selection/script.js
+++ [05] Javascript & DOM/Latihan2-DOM Selection/script.js
@@ -1,12 +1,12 @@
 // DOM Selection
 // document.getElementById() --> Element
-const judul = document.getElementById('judul');
+/* const judul = document.getElementById('judul');
 judul.style.color = 'red';
 judul.style.backgroundColor = '#ccc';
-judul.innerHTML = 'Fadhlillah';
+judul.innerHTML = 'Fadhlillah'; */
 
 // documents.getElementByTagName() --> HTML Collection
-const p = document.getElementsByTagName('p');
+/* const p = document.getElementsByTagName('p');
 
 for(let i = 0; i < p.length; i++){
     p[i].style.backgroundColor = 'lightblue';
@@ -14,9 +14,33 @@ for(let i = 0; i < p.length; i++){
 
 const h1 = document.getElementsByTagName('h1')[0];
 h1.style.fontSize = '20px';
-
-
+ */
 // documents.getElementByClassName() --> HTML Collection
 
-const p1 = document.getElementsByClassName('p1');
-p1[0].innerHTML = 'Ini diubah dari js';
+/* const p1 = document.getElementsByClassName('p1');
+p1[0].innerHTML = 'Ini diubah dari js'; */
+
+// // document.querySelector() --> Element
+// const p4 = document.querySelector('#b p');
+// p4.style.color = 'green';
+// p4.style.fontSize = '15px';
+
+// const li2 = document.querySelector('#b ul li:nth-child(2)');
+// li2.style.backgroundColor = 'orange';
+
+// // document.querySelectorAll() -->
+// const p = document.querySelectorAll('p');
+
+// for(let i = 0; i < p.length; i++){
+//     p[i].style.backgroundColor = 'lightblue';
+// }
+
+// node root
+const sectionB = document.querySelector('#b');
+
+const p4 = sectionB.getElementsByTagName('p')[0];
+
+p4.style.backgroundColor = 'orange';
+
+
+
